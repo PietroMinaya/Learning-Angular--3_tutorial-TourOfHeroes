811644776972ba91d5fd1784ef765ef50c161dca
Pietro <pietorminaya@gmail.com>
2024-01-06 13:51:15 -0500

    Second Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+
 })
 export class AppComponent {
-  title = '3_tutorial-TourOfHeroes';
+  title = 'Tour of Heroes';
 }
